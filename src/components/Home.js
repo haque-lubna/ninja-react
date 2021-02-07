5cf79541b50cc4e27f21c37d66d6e10f2cdfcbe7
lubna40 <lubu7035@gmail.com>
2021-02-07 15:50:51 +0600

    Filter blog

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,12 +5,13 @@ const Home = () => {
     const [blogs, setBlog] = useState([
         {title: "title1", body: "body1", author: "author1", id: 1},
         {title: "title2", body: "body2", author: "author2", id: 2},
-        {title: "title3", body: "body3", author: "author3", id: 3}
+        {title: "title3", body: "body3", author: "author1", id: 3}
     ]);
 
     return (
         <div className="home">
             <BlogList blogs={blogs} title="Blog List are shown below:"/>
+            <BlogList blogs={blogs.filter((blog) => blog.author === "author1")} title="Author1's Blog List are shown below:"/>
         </div>
     );
 }
