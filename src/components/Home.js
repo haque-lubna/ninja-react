8f1c9b8fa70d84de6cbc62825a31e66970a6094a
lubna40 <lubu7035@gmail.com>
2021-02-07 14:50:40 +0600

    Change two variable

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,14 +2,17 @@ import {useState} from 'react';
 
 const Home = () => {
     const [name, setName] = useState("Rahim");
+    const [age, setAge] = useState(20);
+
     const handleClick = () => {
         setName("Karim");
+        setAge(30);
     }
 
     return (
         <div className="home">
             <h2>Homepage</h2>
-            <p>{name}</p>
+            <p>{name} is {age} years old.</p>
             <button onClick={handleClick}>ClickMe</button>
         </div>
     );
