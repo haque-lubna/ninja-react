62b75a9185be24c8a0f59a1359528edd3bc1f70c
lubna40 <lubu7035@gmail.com>
2021-02-07 17:58:25 +0600

    Catch error from response

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,6 +6,8 @@ const Home = () => {
 
     const [isPending, setIsPending] = useState(true);
 
+    const [hasError, setError] = useState(null);
+
     const deleteHandle = (id) => {
         const filteredBlogs = blogs.filter(blog => blog.id !== id);
         setBlog(filteredBlogs);
@@ -15,17 +17,25 @@ const Home = () => {
         setTimeout(() => {
             fetch('http://localhost:3001/blogs')
             .then(res => {
+                if(!res.ok){
+                    throw Error('Something wents wrong!');
+                }
                 return res.json();
             })
             .then(data => {
                 console.log(data);
                 setBlog(data);
                 setIsPending(false);
+            })
+            .catch(err => {
+                setIsPending(false);
+                setError(err.message);
             });
         }, 1000);
     }, []);
     return (
         <div className="home">
+            {hasError && <div style={{backgroundColor: "red"}}>{hasError}</div>}
             {isPending && <div>Loading data......</div>}
             {blogs && <BlogList blogs={blogs} title="Blog List are shown below:" deleteHandle={deleteHandle}/>}
             {blogs && <BlogList blogs={blogs.filter((blog) => blog.author === "Author1")} 
