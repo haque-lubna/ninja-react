47349f0cbaf75fa96ef4c1554a171a71ad0ebd8b
lubna40 <lubu7035@gmail.com>
2021-02-07 14:10:16 +0600

    Try to change variable without state

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,15 @@
 const Home = () => {
-    const handleClick = (event) => {
-        console.log("Clicked Home button", event);
+    let name = "Rahim";
+    const handleClick = () => {
+        name = "Karim";
+        console.log(name);
     }
 
-    const handleClickAgain= (name, event) => {
-        console.log('Hello ' + name, event.target);
-    }
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <p>{name}</p>
             <button onClick={handleClick}>ClickMe</button>
-            <button onClick={(event) => handleClickAgain("Rahim", event) }>ClickMe Again</button>
         </div>
     );
 }
