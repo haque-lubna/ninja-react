56b88187a819cdfd9443283a65971e19ba513d7e
lubna40 <lubu7035@gmail.com>
2021-02-07 15:12:46 +0600

    Show list of blog

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,21 @@
 import {useState} from 'react';
 
 const Home = () => {
-    const [name, setName] = useState("Rahim");
-    const [age, setAge] = useState(20);
-
-    const handleClick = () => {
-        setName("Karim");
-        setAge(30);
-    }
+    const [blogs, setBlog] = useState([
+        {title: "title1", body: "body1", author: "author1", id: 1},
+        {title: "title2", body: "body2", author: "author2", id: 2},
+        {title: "title3", body: "body3", author: "author3", id: 3}
+    ]);
 
     return (
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{name} is {age} years old.</p>
-            <button onClick={handleClick}>ClickMe</button>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2> Title: {blog.title}</h2>
+                    <p>Body: {blog.body}</p>
+                    <p>Written By: {blog.author}</p>
+                </div>
+            ))}
         </div>
     );
 }
