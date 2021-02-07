1d3b34b41c0b7d0639cd96acf5f1f56b40774bb2
lubna40 <lubu7035@gmail.com>
2021-02-07 16:48:16 +0600

    Add useEffect dependency

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,6 +8,8 @@ const Home = () => {
         {title: "title3", body: "body3", author: "author1", id: 3}
     ]);
 
+    const [name, setname] = useState('Rahim');
+
     const deleteHandle = (id) => {
         const filteredBlogs = blogs.filter(blog => blog.id !== id);
         setBlog(filteredBlogs);
@@ -16,13 +18,16 @@ const Home = () => {
     useEffect(() => {
         console.log("Enter into effect");
         console.log(blogs); 
-    });
+        console.log(name);
+    }, [name]);
     return (
         <div className="home">
             <BlogList blogs={blogs} title="Blog List are shown below:" deleteHandle={deleteHandle}/>
             <BlogList blogs={blogs.filter((blog) => blog.author === "author1")} 
                         title="Author1's Blog List are shown below:"
                         deleteHandle={deleteHandle}/>
+            <button onClick = {() => setname("Karim")}>Show Name</button>
+            <p>{name}</p>
         </div>
     );
 }
