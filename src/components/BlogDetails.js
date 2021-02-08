9a43b2cbd5becefb80ccd6d01cf77d0ae900259e
lubna40 <lubu7035@gmail.com>
2021-02-08 14:00:24 +0600

    Add delete action blog

--- src/components/BlogDetails.js
+++ src/components/BlogDetails.js
@@ -1,9 +1,19 @@
-import { useParams } from "react-router";
+import { useHistory, useParams } from "react-router";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     const {id} = useParams();
     const {data: blog, isPending, hasError} = useFetch('http://localhost:3001/blogs/' + id);
+    const history = useHistory();
+
+    const handleDeleteButton = event => {
+        fetch('http://localhost:3001/blogs/' + id, {
+            method: 'DELETE'
+        }).then(() => {
+            console.log("Delete blog with id: " + id);
+            history.push('/');
+        })
+    }
 
     return (
         <div className="blog-details">
@@ -14,6 +24,7 @@ const BlogDetails = () => {
                     <h2>Title: {blog.title}</h2>
                     <p>Author: {blog.author}</p>
                     <p>Body: {blog.body}</p>
+                    <button onClick={handleDeleteButton} style={{backgroundColor: "red"}}>X</button>
                 </article>
             )}
         </div>
