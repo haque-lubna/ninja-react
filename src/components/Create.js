45ba5a63701a86119cbffd5f7c9c52cd3a36d61f
lubna40 <lubu7035@gmail.com>
2021-02-08 12:46:12 +0600

    Create new blog form

--- src/components/Create.js
+++ src/components/Create.js
@@ -2,6 +2,21 @@ const Create = () => {
     return (
         <div className="create">
             <h2>Create new blog</h2>
+            <form>
+                <label>Blog Title:</label>
+                <input type="text" placeholder="Write Blog Title here..." required/>
+
+                <label>Blog Author:</label>
+                <select>
+                    <option value="rahim">Rahim</option>
+                    <option value="karim">Karim</option>
+                </select>
+
+                <label>Blog Body:</label>
+                <textarea placeholder="Write Blog Body here..." required/>
+
+                <button>Add Blog</button>
+            </form>
         </div> 
     );
 }
