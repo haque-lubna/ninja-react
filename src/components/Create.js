a822481999d1d7d3d2c48cc1c4b226d84ee8a145
lubna40 <lubu7035@gmail.com>
2021-02-08 12:59:28 +0600

    Define state for form field

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,19 +1,29 @@
+import { useState } from "react";
+
 const Create = () => {
+const [title, setTitle] = useState('');
+const [author, setAuthor] = useState('');
+const [body, setBody] = useState('');
+
+
     return (
         <div className="create">
             <h2>Create new blog</h2>
             <form>
                 <label>Blog Title:</label>
-                <input type="text" placeholder="Write Blog Title here..." required/>
+                <input type="text" placeholder="Write Blog Title here..." required 
+                value={title} onChange={(event) => setTitle(event.target.value)}/>
 
                 <label>Blog Author:</label>
-                <select>
+                <select value={author} required onChange={(event) => setAuthor(event.target.value)}>
+                    <option value="">Choose Author</option>
                     <option value="rahim">Rahim</option>
                     <option value="karim">Karim</option>
                 </select>
 
                 <label>Blog Body:</label>
-                <textarea placeholder="Write Blog Body here..." required/>
+                <textarea placeholder="Write Blog Body here..." required 
+                value={body} onChange={(event) => setBody(event.target.value)}/>
 
                 <button>Add Blog</button>
             </form>
